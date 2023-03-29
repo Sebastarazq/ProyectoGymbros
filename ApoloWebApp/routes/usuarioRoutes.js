27dbacca6c56437b0c5e7c0fefaefd10cf93c1ef
Sebastarazq <juansebastianhijomenor@hotmail.com>
2023-03-28 19:52:18 -0500

    Renderizacion de paginas del login, registro y olvide contraseña

--- ApoloWebApp/routes/usuarioRoutes.js
+++ ApoloWebApp/routes/usuarioRoutes.js
@@ -1,8 +1,16 @@
 import express from "express";
+import { formularioLogin, formularioRegistro, formularioOlvidePassword } from '../controllers/usuarioController.js'
 
 const router = express.Router();
 
+// Routing
+router.get('/login', formularioLogin);
 
+
+
+router.get('/registro', formularioRegistro)
+
+router.get('/olvide-password', formularioOlvidePassword)
 
 
 
