9696ba68704a3d9fc8845d81a59055b4bf65d9e0
Sebastarazq <juansebastianhijomenor@hotmail.com>
2023-03-29 21:59:16 -0500

    Routings de las vistas de olvide password apra recuperarlos

--- ApoloWebApp/routes/usuarioRoutes.js
+++ ApoloWebApp/routes/usuarioRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { formularioLogin, formularioRegistro, formularioOlvidePassword, registrar,
-confirmar } from '../controllers/usuarioController.js'
+confirmar, resetPassword, comprobarToken, nuevoPassword } from '../controllers/usuarioController.js'
 
 const router = express.Router();
 
@@ -8,13 +8,18 @@ const router = express.Router();
 router.get('/login', formularioLogin);
 
 
-
 router.get('/registro', formularioRegistro)
 router.post('/registro', registrar)
+
 
 router.get('/confirmar/:token', confirmar)
 
 router.get('/olvide-password', formularioOlvidePassword)
+router.post('/olvide-password', resetPassword)
+
+//Almacena el nuevo password
+router.get('/olvide-password/:token', comprobarToken);
+router.post('/olvide-password/:token', nuevoPassword);
 
 
 
