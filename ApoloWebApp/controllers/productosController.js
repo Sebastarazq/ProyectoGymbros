6ed8ce7917d1e3449e8d00728f2d1cfe2772d10a
Sebastarazq <juansebastianhijomenor@hotmail.com>
2023-04-14 19:08:28 -0500

    Formulario de envio de productos completado y guardado en la db y script de proteger ruta y subir Imagenes

--- ApoloWebApp/controllers/productosController.js
+++ ApoloWebApp/controllers/productosController.js
@@ -1,4 +1,5 @@
-import ClaseProducto from "../models/ClaseProducto.js"
+import { validationResult } from 'express-validator'
+import { ClaseProducto, Producto } from '../models/index.js'
 
 const admin = (req,res) => {
     res.render('productos/admin', {
@@ -22,7 +23,118 @@ const crear =  async (req,res) => {
     })
 }
 
+const guardar =  async (req, res) => {
+    // Validacion
+
+    let resultado = validationResult(req)
+
+    if(!resultado.isEmpty()){
+
+        //Consultar Modelo de la clase del producto
+        const [claseProductos]= await Promise.all([
+            ClaseProducto.findAll()
+        ])
+        return  res.render ('productos/crear' ,{
+            pagina: 'Crear Propiedad',
+            csrfToken: req.csrfToken(),
+            claseProductos,
+            errores: resultado.array(),
+            datos: req.body
+        })
+    }
+    //Crear registro
+    const { nombre, descripcion, precio, cantidad, clase: claseProductoId } = req.body
+
+    const { id: usuarioId } = req.usuario
+
+    try {
+        const productoGuardado = await Producto.create({
+            nombre,
+            descripcion,
+            precio,
+            cantidad,
+            claseProductoId,
+            usuarioId,
+            imagen: ''
+        })
+
+        const {id}= productoGuardado
+
+        res.redirect(`/productos/agregar-imagen/${id}`)
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const agregarImagen = async (req,res) => {
+
+    const {id} = req.params
+
+    //Validar que el producto exista
+    const producto =  await Producto.findByPk(id)
+
+    if(!producto){
+        return res.redirect('/admin-productos')
+    }
+
+    //validar que el producto no este publicado
+    if(producto.publicado){
+        return res.redirect('/admin-productos')
+    }
+
+    //validar que el producto pertenece a quien visita esta pagina
+    if(req.usuario.id.toString() !== producto.usuarioId.toString()){
+        return res.redirect('/admin-productos')
+    }
+
+    res.render('productos/agregar-imagen', {
+        pagina:`Agregar Imagen ${producto.nombre}`, 
+        csrfToken: req.csrfToken(),
+        producto
+    })
+}
+
+const almacenarImagen = async (req, res, next) =>{
+    const {id} = req.params
+    
+    //Validar que el producto exista
+    const producto =  await Producto.findByPk(id)
+
+    if(!producto){
+        return res.redirect('/admin-productos')
+    }
+
+    //validar que el producto no este publicado
+    if(producto.publicado){
+        return res.redirect('/admin-productos')
+    }
+
+    //validar que el producto pertenece a quien visita esta pagina
+    if(req.usuario.id.toString() !== producto.usuarioId.toString()){
+        return res.redirect('/admin-productos')
+    }
+
+    try {
+        //console.log(req.file)
+        //Almacenar la imagen y publicar producto
+        producto.imagen = req.file.filename
+        producto.publicado = 1
+        
+        await producto.save()
+        
+        next()
+
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
 export {
     admin,
-    crear
+    crear,
+    guardar,
+    agregarImagen,
+    almacenarImagen
 }
--- ApoloWebApp/routes/productosRoutes.js
+++ ApoloWebApp/routes/productosRoutes.js
@@ -1,10 +1,35 @@
 import express from "express";
-import { admin, crear } from '../controllers/productosController.js'
+import{ body } from 'express-validator'
+import { admin, crear, guardar, agregarImagen, almacenarImagen } from '../controllers/productosController.js'
+import protegerRuta from "../middleware/protegerRuta.js"
+import upload from "../middleware/subirImagen.js"
 
 const router = express.Router()
 
-router.get('/admin-productos',admin)
-router.get('/productos/crear', crear)
+router.get('/admin-productos',protegerRuta, admin)
+router.get('/productos/crear',protegerRuta, crear)
+router.post('/productos/crear',
+protegerRuta,
+body('nombre').notEmpty().withMessage('El titulo del anuncio es obligatorio'),
+body('descripcion')
+.notEmpty().withMessage('La descripcion no puede ir vacia')
+.isLength({ max:300 }).withMessage('La descripcion es muy larga'),
+body('precio').notEmpty().withMessage('El Precio es obligatorio'),
+body('cantidad').notEmpty().withMessage('La cantidad es obligatorio'),
+body('clase').isNumeric().withMessage('Selecciona una clase'),    
+guardar
+)
+
+router.get('/productos/agregar-imagen/:id',
+protegerRuta, 
+agregarImagen
+)
+
+router.post('/productos/agregar-imagen/:id', 
+    protegerRuta,
+    upload.single('imagen'),
+    almacenarImagen
+)
 
 
 
