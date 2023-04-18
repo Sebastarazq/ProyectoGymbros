c578a117a38f8e12da751315c31382c275b14b19
Sebastarazq <juansebastianhijomenor@hotmail.com>
2023-04-17 19:04:05 -0500

    Actualizacion de tiempo del JWT

--- ApoloWebApp/helpers/tokens.js
+++ ApoloWebApp/helpers/tokens.js
@@ -1,10 +1,17 @@
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 
-const generarJWT = datos => jwt.sign({ id : datos.id , nombre: datos.nombre }, process.env.JWT_SECRET, { expiresIn: '1d' })
+const generarJWT = datos => {
+  try {
+    return jwt.sign({ id: datos.id, nombre: datos.nombre }, process.env.JWT_SECRET, { expiresIn: '7d' });
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
 
 const generaId = () => Math.random().toString(32).substring(2) + Date.now().toString(32);
 
-export{
-    generarJWT,
-    generaId
-}
+export {
+  generarJWT,
+  generaId
+};
